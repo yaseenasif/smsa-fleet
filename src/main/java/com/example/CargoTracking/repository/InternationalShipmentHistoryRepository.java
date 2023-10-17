a14bbad70a1c9fed858e536a44db3345d5c1d6f7
yaseenasif <yaseenasif024@gmail.com>
2023-10-17 20:07:15 +0500

    setup

--- src/main/java/com/example/CargoTracking/repository/InternationalShipmentHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.CargoTracking.repository;
-
-import com.example.CargoTracking.model.InternationalShipmentHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InternationalShipmentHistoryRepository extends JpaRepository<InternationalShipmentHistory, Long> {
-}
