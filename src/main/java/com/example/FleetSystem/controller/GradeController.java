2aa3c1fa7c26d585de135ad342056663dbf2a682
Shahzad Iqbal <shahzad.iqbal.developer@gmail.com>
2023-11-16 09:31:37 -0800

    grade crud

---
+++ src/main/java/com/example/FleetSystem/controller/GradeController.java
@@ -0,0 +1,48 @@
+package com.example.FleetSystem.controller;
+
+import com.example.FleetSystem.dto.GradeDto;
+import com.example.FleetSystem.service.GradeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class GradeController {
+
+    @Autowired
+    GradeService gradeService;
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/addGrade")
+    public ResponseEntity<GradeDto> addGrade(@RequestBody GradeDto gradeDto){
+        return ResponseEntity.ok(gradeService.addGrade(gradeDto));
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/get-active-grades")
+    public ResponseEntity<List<GradeDto>> getActiveGrades(){
+        return ResponseEntity.ok(gradeService.getActiveGrades());
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/get-grade/{id}")
+    public ResponseEntity<GradeDto> getGradesById(@PathVariable Long id){
+        return ResponseEntity.ok(gradeService.getById(id));
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/update-grade/{id}")
+    public ResponseEntity<GradeDto> updateGradeById(@PathVariable Long id, @RequestBody GradeDto gradeDto){
+        return ResponseEntity.ok(gradeService.updateGradeById(id,gradeDto));
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/delete-grade/{id}")
+    public ResponseEntity<GradeDto> deleteGradeById(@PathVariable Long id){
+        return ResponseEntity.ok(gradeService.deleteById(id));
+    }
+}
---
+++ src/main/java/com/example/FleetSystem/model/Grade.java
@@ -0,0 +1,25 @@
+package com.example.FleetSystem.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@Entity
+public class Grade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer vehicleBudget;
+    private boolean status;
+}
