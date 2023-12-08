cd67f5e749742ceae310672efda3e9dd038cf9cf
shahzadiqbalpairdesk <shahzad@pairdesk.com>
2023-12-08 23:05:46 +0500

    pre dep

--- src/test/java/com/example/FleetSystem/CargoTrackingApplicationTests.java
+++ src/test/java/com/example/FleetSystem/CargoTrackingApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.FleetSystem;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CargoTrackingApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.FleetSystem;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CargoTrackingApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
