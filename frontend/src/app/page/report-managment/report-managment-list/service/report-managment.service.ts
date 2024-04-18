6c2db5f43e5e423cfebf7070bd8b01197b25b760
Usama Nadeem <usamanadeemparacha@gmail.com>
2024-04-19 01:40:25 +0500

    Changes

--- frontend/src/app/page/report-managment/report-managment-list/service/report-managment.service.ts
+++ frontend/src/app/page/report-managment/report-managment-list/service/report-managment.service.ts
@@ -16,14 +16,25 @@ export class ReportManagmentService {
     private http: HttpClient
   ) { }
 
-  searchVehiclesWithDynamicValues(vehicleInterFace: Vehicle, stringifyPoNumbers?: string,
+  // searchVehiclesWithDynamicValues(vehicleInterFace: Vehicle, stringifyPoNumbers?: string,
+  //   value?: number | null, query?: { page: number, size: number }): Observable<PaginatedResponse<Vehicle>> {
+  //   const params = new HttpParams().set('stringifyPoNumbers', stringifyPoNumbers ? stringifyPoNumbers : '');
+  //   if (value) {
+  //     query = { page: 0, size: 10 };
+  //   }
+  //   return this.http.post<PaginatedResponse<Vehicle>>(`${this.url}/dynamic-search`, vehicleInterFace, { params });
+  // }
+  searchVehiclesWithDynamicValues(vehicleInterface: Vehicle, stringifyPoNumbers?: string,
     value?: number | null, query?: { page: number, size: number }): Observable<PaginatedResponse<Vehicle>> {
-    const params = new HttpParams().set('stringifyPoNumbers', stringifyPoNumbers ? stringifyPoNumbers : '');
+    let params = new HttpParams().set('stringifyPoNumbers', stringifyPoNumbers ? stringifyPoNumbers : '');
     if (value) {
-      query = { page: 0, size: 10 };
+      params = params.set('page', '0');
+      params = params.set('size', '10');
+    } else {
+      params = params.set('page', (query && query.page) ? query.page.toString() : '0');
+      params = params.set('size', (query && query.size) ? query.size.toString() : '10');
     }
-    return this.http.post<PaginatedResponse<Vehicle>>(`${this.url}/dynamic-search`, vehicleInterFace, { params });
+    return this.http.post<PaginatedResponse<Vehicle>>(`${this.url}/dynamic-search`, vehicleInterface, { params });
   }
-
 
 }
